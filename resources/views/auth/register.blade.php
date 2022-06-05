abc5bf8f61d6cba54c1cf3d239a82ebd21212b73
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-06-05 22:57:08 +1000

    :bug: added twitter redirect

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,7 +26,7 @@
 
                     <div class="mt-6 grid grid-cols-1 gap-3">
                         <div>
-                            <a href="#"
+                            <a href="/auth/twitter/redirect"
                                 class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                                 <span class="sr-only">Sign in with Twitter</span>
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
