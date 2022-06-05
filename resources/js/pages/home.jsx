bc999c586e31fe59bc97a51dbe93c8789dfe3809
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-06-05 23:05:41 +1000

    :bug: moved most meta tags to php app file

--- resources/js/pages/home.jsx
+++ resources/js/pages/home.jsx
@@ -57,57 +57,6 @@ const Home = () => {
                     name="description"
                     content="Mates Motivate is the next thing in following your goals, we have found in our own lives that the best motivator there is “your mates”. Encourage a stronger commitment, celebrate your wins, and keep you motivated toward completing your goals."
                 />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://matesmotivate.com/" />
-                <meta
-                    property="og:title"
-                    content="Mates Motivate | Achieve your goals, celebrate success
-                    together."
-                />
-                <meta
-                    property="og:description"
-                    content="Mates Motivate is the next thing in following your goals, we have found in our own lives that the best motivator there is “your mates”. Encourage a stronger commitment, celebrate your wins, and keep you motivated toward completing your goals."
-                />
-                <meta
-                    property="og:image"
-                    content="https://matesmotivate.com/assets/meta.jpg"
-                />
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta
-                    property="twitter:url"
-                    content="https://matesmotivate.com/"
-                />
-                <meta
-                    property="twitter:title"
-                    content="Mates Motivate | Achieve your goals, celebrate success
-                    together."
-                />
-                <meta
-                    property="twitter:description"
-                    content="Mates Motivate is the next thing in following your goals, we have found in our own lives that the best motivator there is “your mates”. Encourage a stronger commitment, celebrate your wins, and keep you motivated toward completing your goals."
-                />
-                <meta
-                    property="twitter:image"
-                    content="https://matesmotivate.com/assets/meta.jpg"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon-16x16.png"
-                />
-                <link rel="manifest" href="/site.webmanifest"></link>
             </Head>
 
             {/* Hero */}
